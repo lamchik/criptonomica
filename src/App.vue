d5ca350659a5879ee315ec44dcc1aaa36f137dbf
lamara <lamaralanchava@gmail.com>
2024-01-07 02:37:24 +0300

    fixed styles

--- src/App.vue
+++ src/App.vue
@@ -32,14 +32,14 @@
             <label for="wallet" class="block text-sm font-medium text-gray-700"
               >Тикер</label
             >
-            <div class="mt-1 relative rounded-md shadow-md">
+            <div class="mt-1 relative rounded-md shadow-md mb-1">
               <input
                 v-model="ticker"
                 @keydown.enter="add"
                 type="text"
                 name="wallet"
                 id="wallet"
-                class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+                class="p-2.5 block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
                 placeholder="Например DOGE"
               />
             </div>
